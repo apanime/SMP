49cc77b02c913c5d12f3b6f4af1df354b8c37382
Animesh Pant <Animesh Pant@DESKTOP-VH9EHO0>
2019-09-04 07:45:09 +0530

    First commit

---
+++ Ani/src/main/java/com/ani/Ani/App.java
@@ -0,0 +1,13 @@
+package com.ani.Ani;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
